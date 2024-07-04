698affb18042f5e6ab2b656a767b7db7d6211794
alex garag <alex.garag@bk.ru>
2024-07-04 11:19:11 +0300

    переделка BaseConverter

--- src/by/tms/lesson9c30onl/convert/BaseConverter.java
+++ src/by/tms/lesson9c30onl/convert/BaseConverter.java
@@ -2,8 +2,8 @@ package by.tms.lesson9c30onl.convert;
 
 import by.tms.lesson9c30onl.convert.interfaces.Convertible;
 
-public abstract class BaseConverter implements Convertible {
-    @Override
+public class BaseConverter {
+
     public double convert(double convertedValue) {
         return 0;
     }
